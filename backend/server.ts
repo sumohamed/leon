b2255bc1fc438a68a9ac4c4125a788af20f0f109
Sally Mohamed <sally.mohamed110@gmail.com>
2026-05-29 18:30:13 +0300

    Setup complete with typescript, db connect and vercel config

---
+++ backend/server.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI || "";
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+	.connect(MONGO_URI)
+	.then(() => {
+		console.log("Successfully connected to MongoDB Atlas! 🍃");
+	})
+	.catch((error) => {
+		console.error("Error connecting to MongoDB:", error.message);
+	});
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("Leon Template API is running successfully! 🚀");
+});
+
+app.listen(PORT, () => {
+	console.log(`[nodemon] Server is running on port ${PORT}`);
+});
